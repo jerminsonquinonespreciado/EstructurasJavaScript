6f5503ee4d6056284bfc484e0155f46cba21dece
Jerminson Quiñones Preciado <jerminsonquinonespreciado@gmail.com>
2026-07-05 11:54:19 -0500

    Rediseño de la aplicación 1

--- app1/script.js
+++ app1/script.js
@@ -1,52 +1,38 @@
-let animales = [
-    "Perro",
-    "Gato",
-    "Caballo",
-    "León",
-    "Conejo"
-];
+let arreglo = ["Manzana", "Banano", "Pera"];
 
-function agregarInicio(){
+function imprimirArray() {
+    document.getElementById("resultado").innerHTML = `
+        <strong>Contenido del Array:</strong><br><br>
+        ${arreglo.join(" | ")}
+    `;
+}
 
-    let nuevo = prompt("Ingrese un animal");
+function agregarInicio() {
+    const dato = prompt("Ingrese un elemento para agregar al inicio:");
 
-    if(nuevo){
+    if (dato) {
+        arreglo.unshift(dato);
+        imprimirArray();
+    }
+}
 
-        animales.unshift(nuevo);
+function agregarFinal() {
+    const dato = prompt("Ingrese un elemento para agregar al final:");
 
+    if (dato) {
+        arreglo.push(dato);
+        imprimirArray();
+    }
+}
+
+function eliminarTercero() {
+    if (arreglo.length >= 3) {
+        arreglo.splice(2, 1);
+    } else {
+        alert("El Array tiene menos de 3 elementos.");
     }
 
+    imprimirArray();
 }
 
-function agregarFinal(){
-
-    let nuevo = prompt("Ingrese un animal");
-
-    if(nuevo){
-
-        animales.push(nuevo);
-
-    }
-
-}
-
-function eliminarTercero(){
-
-    if(animales.length >=3){
-
-        animales.splice(2,1);
-
-    }else{
-
-        alert("No existe un tercer elemento.");
-
-    }
-
-}
-
-function imprimirArray(){
-
-    document.getElementById("resultado").innerHTML =
-    animales.join("<br>");
-
-}
+window.onload = imprimirArray;
